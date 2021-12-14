6b031b9b6e4ee906f3476197dc56e45fd074922f
bayanchen <2650797944@qq.com>
2021-12-14 22:23:46 +0800

    12/14 登录相关功能

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -1,6 +1,8 @@
 package com.nowcoder.community.service;
 
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
@@ -27,6 +29,9 @@ public class UserService implements CommunityConstant {
 
     @Autowired
     private TemplateEngine templateEngine;
+
+    @Autowired(required = false)
+    private LoginTicketMapper loginTicketMapper;
 
     @Value("${community.path.domain}")
     private  String domain;
@@ -77,7 +82,7 @@ public class UserService implements CommunityConstant {
         user.setType(0);
         user.setStatus(0);
         user.setActivationCode(CommunityUtil.generateUUID());
-        user.setHeaderUrl(String.format("http:images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
         user.setCreateTime(new Date());
         userMapper.insertUser(user);
 
@@ -105,5 +110,61 @@ public class UserService implements CommunityConstant {
             return ACTIVATION_FAILURE;
         }
     }
+
+    public Map<String,Object> login(String username,String password,long expiredSeconds){
+        Map<String,Object> map=new HashMap<>();
+        //空值处理
+        if(StringUtils.isBlank(username)){
+            map.put("usernameMsg","账号不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(password)){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+        //验证账号
+        User user=userMapper.selectByName(username);
+        if(user==null){
+            map.put("usernameMsg","该账号不存在");
+            return map;
+        }
+        //验证状态
+        if(user.getStatus()==0){
+            map.put("usernameMsg","该账号未激活");
+            return map;
+        }
+        //验证密码
+        password=CommunityUtil.md5(password+user.getSalt());
+        if(!user.getPassword().equals(password)){
+            map.put("passwordMsg","密码不正确");
+            return map;
+        }
+        //生成登录凭证
+        LoginTicket loginTicket=new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+ expiredSeconds*1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+        //
+        map.put("ticket",loginTicket.getTicket());
+        return map;
+    }
+    //退出
+    public void logout(String ticket){
+        //1表示无效
+        loginTicketMapper.updateStatus(ticket,1);
+    }
+
+    //查询凭证
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+    //修改头像路径
+    public int updateHeader(int userId,String headerUrl){
+        return userMapper.updateHeader(userId,headerUrl);
+    }
+
 
 }
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -8,5 +8,11 @@ public interface CommunityConstant {
     //激活失败
     int ACTIVATION_FAILURE=2;
 
+    //默认状态登录凭证超时时间
+    int DEFAULT_EXPIRED_SECONDS=3600*12;
+
+    //记住状态下的登录超时时间
+    int REMEMBER_EXPIRED_SECONDS=3600*24*100;
+
 
 }
